7c2461eea520000bfb3dc06fc99ab9d7b52fae79
aquinn17 <aileen.r.quinn@gmail.com>
2018-02-12 19:51:26 -0500

    Add files via upload

---
+++ family_data.sql
@@ -0,0 +1,6 @@
+﻿SET IDENTITY_INSERT PERSON ON
+INSERT INTO [Person] ([Id],[Name],[Gender],[Birthday],[Age], [Alive], [Death], [Child], [Spouse])VALUES('I01','Joe Smith','M','1960-07-15','53','2013-12-31', 'NA', '{F23}')
+SET IDENTITY_INSERT Person OFF
+SET IDENTITY_INSERT Family ON
+INSERT INTO [Family] ([Id],[Married],[Divorced],[Husband_ID],[Husband_Name],[Wife_ID],[Wife_Name], [Children])VALUES('F23','1980-02-14','NA','I01','Joe Smith','I07', 'Jennifer Smith', '{I26}, {I19}')
+SET IDENTITY_INSERT Family OFF
